1559ae7e4e49f782f0534b173b6be757f19ecfb0
Yechiel <mhb@paritcher.com>
2024-05-23 16:55:04 -0400

    record color

--- resources/views/record.blade.php
+++ resources/views/record.blade.php
@@ -2,7 +2,7 @@
 <div
     id="{{ $record['id'] }}"
     @if ($recordClickEnabled) wire:click="onRecordClick('{{ $record['id'] }}')" @endif
-    class="{{ $styles['record'] }}">
+    class="{{ $styles['record'] }} {{ isset($record['color']) ? 'b-' . $record['color'] : null }}">
 
     @include($recordContentView, [
         'record' => $record,
